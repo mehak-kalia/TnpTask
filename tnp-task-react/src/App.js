0c188cbeb998702ddd09967241740f121d74d443
mehak-kalia <mehakkalia77@gmail.com>
2022-09-24 07:38:48 +0530

    Routing Attempt

--- tnp-task-react/src/App.js
+++ tnp-task-react/src/App.js
@@ -8,47 +8,25 @@ import Navbar from './components/Navbar/navbarIndex';
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
 function App() {
-//   return (
-//     // <Router>
-//     // <Navbar />
-//     // {/* <Routes>
-//     //     <Route exact path='/' element={<Home />} />
-//     //     <Route path='/post' element={<Posts/>} />
-//     // </Routes> */}
-//     // </Router>
+  return (
+    // <Router>
+    // <Navbar />
+    // {/* <Routes>
+    //     <Route exact path='/' element={<Home />} />
+    //     <Route path='/post' element={<Posts/>} />
+    // </Routes> */}
+    // </Router>
+<div className='App'>
+<Router>
+    <Navbar />
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/post' element={<Posts/>} />
+    </Routes>
+    </Router>
+</div>
+);
 
-//     <Navbar>
-
-//     </Navbar>
-// );
-
-const [posts, setPosts] = useState([])
-    useEffect(()=>{
-      async function getAllPosts(){
-        try {
-          const posts = await axios.get("http://127.0.0.1:8000/api/post/")
-          console.log(posts.data)
-          setPosts(posts.data)
-        } catch (error) {
-          console.log(error)
-        }
-      }
-      getAllPosts()
-    }, [])
-  
-  
-    return (
-      <div className="App">
-       <h1>Connect React JS with Laravel</h1>
-       {
-         posts.map((post, i)=>{
-           return (
-             <><h2 key={i}>{post.posthead}</h2><h3 key={i}>{post.postsubhead}</h3></>
-           )
-         })
-       }
-      </div>
-    );
 }
 
 export default App;
